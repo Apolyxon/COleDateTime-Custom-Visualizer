8d73387fbac263fe306586dc3ca6d2b4026e80cd
blo <benedikt.lohmann@connext.de>
2019-08-23 11:41:26 +0200

    Rename

---
+++ COleDateTimeCustomVisualizer/dll/dllmain.h
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+class CCOleDateTimeCustomVisualizerModule : public CAtlDllModuleT< CCOleDateTimeCustomVisualizerModule >
+{
+};
+
+extern class CCOleDateTimeCustomVisualizerModule _AtlModule;
